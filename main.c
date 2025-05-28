798e4ed688f7dd96088f5e6b1b64790ac891c36d
victoria.oderman@gmail.com <victoria.oderman@gmail.com>
2025-05-28 18:01:13 +0200

    first dice roll

--- main.c
+++ main.c
@@ -5,6 +5,12 @@ int main()
 {
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
+    int dice_rolls[100];
+    for (int i=0; i<100; i++){
+        dice_rolls[i] = (rand() %6)+1;
+        //check for rolls
+        printf("roll %d\n", dice_rolls[i]);
+    }
 
    
     // Your code goes here
